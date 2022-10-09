8906fefe015913d3df4828c890de673c6ab6742c
zjsun <106876786+zjs919@users.noreply.github.com>
2022-10-09 15:32:50 +0800

    Delete iRML_eval.sh

--- iRML_Reasoning/scripts/iRML_eval.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-relation=$1
-
-python evaluate.py $relation 
-python transR_eval.py $relation
-python transE_eval.py $relation
-python transH_eval.py $relation
-python transD_eval.py $relation
